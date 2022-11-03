da01d0fd0d4081540d16b44ce04cbcf9d4d201aa
m254986 <115638575+m254986@users.noreply.github.com>
2022-11-03 09:48:34 -0400

    learning :D

--- game.py
+++ game.py
@@ -1,1 +1,22 @@
-print("MIGHTY MIDY")
+import pygame
+import time
+import sys
+
+pygame.init()
+
+screen = pygame.display.set_mode((400,400))
+
+while True:
+    #1 check for user input (key press, mouse clicks, etc!)
+    print("------- CHECKING --------")
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEMOTION:
+            print("YOU MOVED THE MOUSE!!")
+        if event.type == pygame.QUIT:
+            sys.exit()
+        #if event.type == pygame.KEYUP:
+            #screen.fill
+    print("------- DONE CHECKING --------")
+
+    pygame.display.flip()
+    time.sleep(1)
